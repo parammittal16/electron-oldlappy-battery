6016bdbf4f7ea5b1179535860889758bef950aac
rishabhscross <parammittal16@gmail.com>
2019-01-04 21:40:08 +0530

    Add tray icon

--- main.js
+++ main.js
@@ -1,38 +1,25 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, Menu, Tray} = require('electron')
 const electron = require('electron')
 const notifier = require('node-notifier');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-  
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-  
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-  
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+let tray = null
 
 app.on('ready', () => {
   electron.powerMonitor.on('on-battery', () => {
+    tray = new Tray('./favicon.ico')
+    tray.setToolTip('Double click to close the application.')
+    tray.on('double-click', () => {
+      if (process.platform !== 'darwin') {
+        app.quit()
+      }
+    })
     notifier.notify({
       title: 'On battery',
       message: 'Plug in fast PARAM'
@@ -41,21 +28,6 @@ app.on('ready', () => {
 })
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
